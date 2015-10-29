63ea154b7be9274d1dbe514d35b0a740b078e455
Justin Hollis <justinhollis@Justins-MacBook-Pro.local>
2015-10-29 09:02:59 -0400

    added a couple cool methods, dude

--- Hollisj PIGO.py
+++ Hollisj PIGO.py
@@ -1,11 +1,34 @@
 #from gopigo import
 
 __author__ = 'justinhollis'
+from gopigo import *
+import time
 
 class Pigo:
+
+    isMoving = False
+    servoPos = 90
+
 
     def __init__(self):
         print "I am alive and ready for war."
 
+    def stop(selfself):
+        self.isMoving = False
+        while stop() != 1:
+            time.sleep(.1)
+            print "No brakes, save me"
+
+    def fwd(selfself):
+        self.isMoving = True
+        while fwd() !=1:
+            time.sleep(.1)
+            print "I ate to much for breakfeast this morning."
+
 bob = Pigo()
+bob.fwd()
+time.sleep(2)
+bob.stop()
+
+
 
